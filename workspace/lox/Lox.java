765105782cee294e91b8d09c49cd39e0438c6146
mutsune <mutsune@users.noreply.github.com>
2026-03-05 13:40:17 +0000

    Add Resolving variable expressions

--- workspace/lox/Lox.java
+++ workspace/lox/Lox.java
@@ -62,6 +62,9 @@ public class Lox {
             return;
         }
 
+        Resolver resolver = new Resolver(interpreter);
+        resolver.resolve(statements);
+
         interpreter.interpret(statements);
     }
 
